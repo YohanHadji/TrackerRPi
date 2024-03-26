9dfdbe4f51b5e938f87c593b1f7de41a952f2b20
YohanHadji <hadji.yohan@gmail.com>
2024-03-26 18:24:43 +0100

    sleep time

--- mightyZap.py
+++ mightyZap.py
@@ -8,22 +8,27 @@ MightyZap = PythonLibMightyZap_Rasp_PC
 Actuator_ID = 1
 
 MightyZap.OpenMightyZap('/dev/ttyAMA0',57600)
-time.sleep(0.1)
+time.sleep(1.0)
 pos =0
 
-MightyZap.GoalPosition(Actuator_ID,3000)
-while pos < 2990 :
-    try:
-        pos = MightyZap.PresentPosition(Actuator_ID)
-    except serial.SerialException as e:
-        print("Serial read issue:", e)
-    print(pos)
+while True: 
+    MightyZap.GoalPosition(Actuator_ID,3000)
+    while pos < 2990 :
+        try:
+            pos = MightyZap.PresentPosition(Actuator_ID)
+        except serial.SerialException as e:
+            print("Serial read issue:", e)
+        print(pos)
+        time.sleep(0.1)
+    time.sleep(2.0)
 
-MightyZap.GoalPosition(Actuator_ID,0)
-while pos > 10 :
-    try:
-        pos = MightyZap.PresentPosition(Actuator_ID)
-    except serial.SerialException as e:
-        print("Serial read issue:", e)
-    print(pos) 
+    MightyZap.GoalPosition(Actuator_ID,0)
+    while pos > 10 :
+        try:
+            pos = MightyZap.PresentPosition(Actuator_ID)
+        except serial.SerialException as e:
+            print("Serial read issue:", e)
+        print(pos) 
+        time.sleep(0.1)
+    time.sleep(2.0)
     
