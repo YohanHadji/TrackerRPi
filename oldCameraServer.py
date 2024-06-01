da9d54b67391e5f74666b2f413b6d52bc580863f
YohanHadji <hadji.yohan@gmail.com>
2024-06-01 16:08:49 +0200

    old index fix

--- oldCameraServer.py
+++ oldCameraServer.py
@@ -1,25 +1,33 @@
 from flask import Flask, Response
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import cv2
+import io
+import time
 
 app = Flask(__name__)
 
 # Initialize the camera
-camera = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 24
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# Allow the camera to warmup
+time.sleep(0.1)
 
 def gen_frames():
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            # Convert the frame to JPEG
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
+        ret, buffer = cv2.imencode('.jpg', image)
+        frame = buffer.tobytes()
+        
+        # Clear the stream for the next frame
+        rawCapture.truncate(0)
 
-            # Yield frame to Flask response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        # Yield frame to Flask response
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -27,7 +35,7 @@ def video_feed():
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('oldIndex.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
