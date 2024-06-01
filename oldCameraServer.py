5e1e549fd673616648e18f920f28ae1ade36baed
YohanHadji <hadji.yohan@gmail.com>
2024-06-01 19:06:30 +0200

    30deg

--- oldCameraServer.py
+++ oldCameraServer.py
@@ -75,12 +75,12 @@ def index():
 
 @app.route('/zoom_in', methods=['POST'])
 def button1():
-    sendZoom(20)
+    sendZoom(30)
     return '', 204
 
 @app.route('/zoom_out', methods=['POST'])
 def button2():
-    sendZoom(-20)
+    sendZoom(-30)
     return '', 204
 
 @app.route('/record_on', methods=['POST'])
