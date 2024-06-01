8795f3378a74bf879c01e7953139284f1471d23c
YohanHadji <hadji.yohan@gmail.com>
2024-06-01 16:15:50 +0200

    second try

--- oldCameraServer.py
+++ oldCameraServer.py
@@ -1,9 +1,10 @@
-from flask import Flask, Response, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import cv2
 import io
 import time
+import serial
 
 app = Flask(__name__)
 
@@ -15,6 +16,9 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # Allow the camera to warm up
 time.sleep(0.1)
+
+# Initialize serial communication with Arduino
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
 def gen_frames():
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -36,6 +40,26 @@ def video_feed():
 @app.route('/')
 def index():
     return render_template('oldIndex.html')
+
+@app.route('/zoom_in')
+def zoom_in():
+    ser.write(b'zoom_in\n')
+    return redirect(url_for('oldIndex'))
+
+@app.route('/zoom_out')
+def zoom_out():
+    ser.write(b'zoom_out\n')
+    return redirect(url_for('oldIndex'))
+
+@app.route('/record_on')
+def record_on():
+    ser.write(b'record_on\n')
+    return redirect(url_for('oldIndex'))
+
+@app.route('/record_off')
+def record_off():
+    ser.write(b'record_off\n')
+    return redirect(url_for('oldIndex'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
