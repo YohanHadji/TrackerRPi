5cf81ffc3f4c14949effd4d0215e4a005688e9a4
YohanHadji <hadji.yohan@gmail.com>
2024-06-01 16:53:49 +0200

    simpler test

--- oldCameraServer.py
+++ oldCameraServer.py
@@ -1,62 +1,27 @@
-from flask import Flask, render_template, Response, request
-from picamera import PiCamera
-from picamera.array import PiRGBArray
-import cv2
-import io
-import time
-import serial
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Initialize the camera
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 24
-rawCapture = PiRGBArray(camera, size=(640, 480))
+# Function to handle POST requests from buttons
+def function1():
+    print("Function 1 called")
 
-# Allow the camera to warm up
-time.sleep(0.1)
-
-# Initialize serial communication with Arduino
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-
-def gen_frames():
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        ret, buffer = cv2.imencode('.jpg', image)
-        frame = buffer.tobytes()
-        
-        # Clear the stream for the next frame
-        rawCapture.truncate(0)
-
-        # Yield frame to Flask response
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def function2():
+    print("Function 2 called")
 
 @app.route('/')
 def index():
-    return render_template('oldIndex.html')
+    return render_template('index.html')
 
-@app.route('/control', methods=['POST'])
-def control():
-    action = request.form.get('action')
-    if action == 'zoom_in':
-        print('zoom_in')
-        # ser.write(b'zoom_in\n')
-    elif action == 'zoom_out':
-        print('zoom_out')
-        # ser.write(b'zoom_out\n')
-    elif action == 'record_on':
-        print('record_on')
-        # ser.write(b'record_on\n')
-    elif action == 'record_off':
-        print('record_off')
-        # ser.write(b'record_off\n')
-    return render_template('oldIndex.html')
+@app.route('/button1', methods=['POST'])
+def button1():
+    function1()
+    return '', 204
+
+@app.route('/button2', methods=['POST'])
+def button2():
+    function2()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
