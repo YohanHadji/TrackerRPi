18ea573aee7e5717d2832980d0cf8969ef19698c
YohanHadji <hadji.yohan@gmail.com>
2024-06-01 17:01:46 +0200

    picameraarray

--- oldCameraServer.py
+++ oldCameraServer.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, Response
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 import time
 import io
+from PIL import Image
 
 app = Flask(__name__)
 
 camera = PiCamera()
+camera.resolution = (640, 480)
 
-# Function to handle POST requests from buttons
 def zoom_in():
     print("Zoom + called")
 
@@ -16,11 +18,11 @@ def zoom_out():
 
 def record_on():
     print("Record On called")
-    # camera.start_recording('video.h264')
+    camera.start_recording('video.h264')
 
 def record_off():
     print("Record Off called")
-    # camera.stop_recording()
+    camera.stop_recording()
 
 @app.route('/')
 def index():
@@ -52,15 +54,17 @@ def video_feed():
 
 def gen(camera):
     while True:
-        time.sleep(0.1)  # Adjust delay according to your frame rate requirement
         frame = capture_frame(camera)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 def capture_frame(camera):
-    frame = io.BytesIO()
-    camera.capture(frame, format='jpeg', use_video_port=True)
-    return frame.getvalue()
+    raw_capture = PiRGBArray(camera)
+    camera.capture(raw_capture, format='rgb', use_video_port=True)
+    frame = raw_capture.array
+    with io.BytesIO() as output:
+        Image.fromarray(frame).save(output, format="JPEG")
+        return output.getvalue()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
