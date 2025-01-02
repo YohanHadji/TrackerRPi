bfb6c8031d92c7d385169098f526687a22f77619
spectro-pointer <gustavosantamaria@hotmail.com>
2025-01-02 17:10:34 +0000

    actualizado al 01_02_2025

--- canon.py
+++ canon.py
@@ -9,7 +9,9 @@ import cv2
 import numpy as np
 
 # Initialize the webcam (Elgato USB cam link)
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('/dev/video8')
+
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 # cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
